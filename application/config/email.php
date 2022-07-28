ae89c4bb87f2cbd71a3d929decec0829c813b3d4
jittu92 <jitturoy1992@gmail.com>
2022-07-29 02:07:42 +0530

    debug

--- application/config/email.php
+++ application/config/email.php
@@ -4,10 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 if($_SERVER['SERVER_NAME'] == 'localhost'){
   $config = array(
       'protocol' => 'smtp', // 'mail', 'sendmail', or 'smtp'
-      'smtp_host' => 'srv70.cloudserverzone.com',
+      'smtp_host' => '',
       'smtp_port' => 465,
-      'smtp_user' => 'mailer_testing@cryptappz.com',
-      'smtp_pass' => '123456789',
+      'smtp_user' => '',
+      'smtp_pass' => '',
       'smtp_crypto' => 'ssl', // can be 'ssl' or 'tls' for example
       'mailtype' => 'html', // plaintext 'text' mails or 'html'
       'smtp_timeout' => '4', // in seconds
@@ -19,10 +19,10 @@ if($_SERVER['SERVER_NAME'] == 'localhost'){
 else{
   $config = array(
       'protocol' => 'smtp', // 'mail', 'sendmail', or 'smtp'
-      'smtp_host' => 'srv70.cloudserverzone.com',
+      'smtp_host' => '',
       'smtp_port' => 465,
-      'smtp_user' => 'support@saraha.co.in',
-      'smtp_pass' => 'kee27V46%',
+      'smtp_user' => '',
+      'smtp_pass' => '',
       'smtp_crypto' => 'ssl', // can be 'ssl' or 'tls' for example
       'mailtype' => 'html', // plaintext 'text' mails or 'html'
       'smtp_timeout' => '4', // in seconds
